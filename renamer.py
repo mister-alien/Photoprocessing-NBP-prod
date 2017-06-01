3187f598c4b1dcf5cac1610727998282d9a088cf
Chris Logan <cdlogan@gmail.com>
2017-06-01 06:51:09 -0300

    added to script
    no large changes
    
    req - openpyxl
    -python 3.x.x

--- renamer.py
+++ renamer.py
@@ -1,8 +1,15 @@
 from openpyxl import Workbook
 from openpyxl import load_workbook
 
+ind=0
 offset = 2
+
 namecol='A'
+txcol='D'
+platecol='E'
+
 wb = load_workbook('Line_1234.xlsx')
 ws=wb.active
-print(ws['A2'])
+a=ws[platecol +str(offset+ind)]
+print(a.value)
+print(a.value ==None)
