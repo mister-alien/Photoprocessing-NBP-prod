23697df9950fe910b3f7354d90672288e35ab73e
Chris Logan <cdlogan@gmail.com>
2017-06-19 09:52:32 -0300

    Big ease of use update!
    ADDED:
    Config page, this can edit the following parameters:
        -Tx name column
        -Tx picture # columns
        -Plate picture # columns
        -header offset 
        -Image file prefix
        -Defaults can always be reset in software
    Info page 
        -includes simple copyright notice (all me)
        - not much else
    Added icons to replace the default... (didn't take any time on these)
    
    Fair enough to also mention the program feels a lot more like a real program now and less like a random script... fair to list all current dependencies should someone want to run this:
    
        -Python 3.x
        -appJar (pip install appjar)
        -shutils, which may be builtin
        -tkinter
        -os, which is definitely builtin
    
    To do:
       Important:
    C O M M E N T
    need to comment things in additional detail, remove redundant code,etc
         Check for file not found! This can cause the program to crash if you pick the wrong directory instead of tossing a warning.
         Check for generic errors in anythin not gui related... such as validating configuration when you apply/ok, since otherwise you can currently enter any text/characters into the textboxes. (check if offset is number, check if columns are only letters with no spaces/special characters/numbers. (will cause renstuff to throw an error, ugly)
    
    The above config validation will only make sure the entries are possibly valid, but without data validation it cant check if the configuration is actually correct.
        
        Less Important:
         simple data validation.. check the two picture number columns for repeated numbers...
    If there is repeated numbers, report which two cells are repeated, and whether to cancel or continue.
    before that , the program would check that there are only numbers in the cells..
    
        Very unimportant:
         A progress bar based on the index of total rows in the spreadsheet would be nice, if not particularly critical.

--- renamer-gui.py
+++ renamer-gui.py
@@ -1,11 +1,21 @@
 import renamethings
+import os.path
 from renamethings import renstuff
 from appJar import gui
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import askdirectory
+#from __future__ import print_function    (for when i use py 2 )
 
+
+###################################################
+# Action definitions for the button pressing and the menu buttons
 def press(btn):
+    global offset
+    global prefix
+    global namecol
+    global txcol
+    global platecol
     if(btn=="Cancel"):
         app.stop()
     elif(btn=="Browse"):
@@ -13,29 +23,98 @@ def press(btn):
         filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
         app.setEntry("linefile",filename)
     elif(btn=="Rename"):
+        print(str(offset)+'\n'+prefix+'\n'+namecol+'\n'+txcol+'\n'+platecol)
         renstuff(app.getEntry("linefile"),app.getEntry("workdir"),namecol,platecol,txcol,prefix,offset)
         print("Done")    
     elif(btn=="work dir"):
         Tk().withdraw()
         direct=askdirectory()
         app.setEntry("workdir",direct)
+        
+    elif(btn=="Defaults"):
+        cfg=open('config.txt','w+') #If the config doesn't exist, write the default values to it
+        offset = 2
+        prefix='IMG_0000'
+        namecol='A'
+        txcol='L'
+        platecol='M'
+        cfg.write(str(offset)+'\n'+prefix+'\n'+namecol+'\n'+txcol+'\n'+platecol)
+        cfg.close()
+        app.setEntry("pfix", prefix)
+        app.setEntry("ofset", offset)
+        app.setEntry("namcol", namecol)
+        app.setEntry("txco", txcol)
+        app.setEntry("plateco", platecol)
+        
+    elif(btn=="OK"):
+        prefix = app.getEntry("pfix").rstrip()
+        offset = int(app.getEntry("ofset"))
+        namecol = app.getEntry("namcol").rstrip()
+        txcol = app.getEntry("txco").rstrip()
+        platecol = app.getEntry("plateco").rstrip()
+        cfg=open('config.txt','w+')
+        cfg.write(str(offset)+'\n'+prefix+'\n'+namecol+'\n'+txcol+'\n'+platecol)
+        cfg.close()
+        app.hideSubWindow("config")
+    elif(btn=="Apply"):
+        prefix = app.getEntry("pfix")
+        offset = app.getEntry("ofset")
+        namecol = app.getEntry("namcol")
+        txcol = app.getEntry("txco")
+        platecol = app.getEntry("plateco")
+        cfg=open('config.txt','w+')
+        cfg.write(str(offset)+'\n'+prefix+'\n'+namecol+'\n'+txcol+'\n'+platecol)
+        cfg.close()
+    elif(btn=="cancelconf"):
+        app.hideSubWindow("config")
+        
 def menuPress(menus):
     if(menus=="Config"):
-        print("Dont do it")
+        app.showSubWindow("config")
+        
     elif(menus=="Info"):
-        info=gui()
-        info.addLabel("title","Test",0,0,2)
-        info.go()
+        app.showSubWindow("info")
     elif(menus=="Close"):
         app.stop()
         #### Default value block
-offset = 2
-prefix='IMG_0000'
-namecol='A'
-txcol='L'
-platecol='M'
+def confpress(btn):
+    if(btn=="Cancel"):
+        config.stop()
+#
+###################################################
 
-app=gui()
+if os.path.isfile('config.txt'):
+    with open('config.txt','r') as cfg: #if the config file does exist, great! open and parse its values
+        offset=None
+        for line in cfg:
+            if offset==None:
+                offset = int(line)
+                prefix=None
+            elif prefix==None:
+                prefix = line.rstrip()
+                namecol=None
+            elif namecol==None:
+                namecol = line.rstrip()
+                txcol=None
+            elif txcol==None:
+                txcol = line.rstrip()
+                platecol=None
+            elif platecol==None:
+                platecol = line.rstrip()
+            else:
+                print('why are there extra lines?')
+else:
+    cfg=open('config.txt','w+') #If the config doesn't exist, write the default values to it
+    offset = 2
+    prefix='IMG_0000'
+    namecol='A'
+    txcol='L'
+    platecol='M'
+    cfg.write(str(offset)+'\n'+prefix+'\n'+namecol+'\n'+txcol+'\n'+platecol)
+    cfg.close()
+
+print(str(offset)+'\n'+prefix+'\n'+namecol+'\n'+txcol+'\n'+platecol)
+app=gui(title="Photo-Renamer")
 
 ### fillings
 
@@ -46,7 +125,7 @@ app.addLabel("linefile","Spreadsheet Path",1,0)
 app.addEntry("linefile",1,1)
 app.addButton("Browse",press,1,2)
 
-app.addLabel("workdir","Line Folder: ",2,0)
+app.addLabel("workdir","Raw Photo Directory: ",2,0)
 app.addEntry("workdir",2,1)
 app.addButton("work dir",press,2,2)
 
@@ -57,4 +136,42 @@ app.addMenuList("File",fileMenus,menuPress)
 
 app.setEntryFocus("linefile")
 
+app.startSubWindow("config",modal=True,blocking=True)
+app.addLabel("pfix","Raw image prefix: ",1,0)
+app.addEntry("pfix",1,1)
+app.setEntry("pfix", prefix)
+
+app.addLabel("ofset","Spreadsheet header row: ",2,0)
+app.addEntry("ofset",2,1)
+app.setEntry("ofset", str(offset))
+
+app.addLabel("namcol","Tx name column: ",3,0)
+app.addEntry("namcol",3,1)
+app.setEntry("namcol", namecol)
+
+app.addLabel("txco","Tx pic column: ",4,0)
+app.addEntry("txco",4,1)
+app.setEntry("txco", txcol)
+
+app.addLabel("plateco","Plate pic column: ",5,0)
+app.addEntry("plateco",5,1)
+app.setEntry("plateco", platecol)
+
+app.addButton("Defaults",press,6,0,1)
+app.addButton("OK",press,7,0,1)
+app.addButton("Apply",press,7,1,1)
+app.addNamedButton("Cancel","cancelconf",press,7,2,1)
+app.setIcon("config.ico")
+app.stopSubWindow()
+app.hideSubWindow("config")
+
+app.startSubWindow("info",modal=True,blocking=True)
+app.addMessage("infostuff","(c) Christopher Logan, 2017\n \
+             This program is intended only for use in renaming  \
+             raw data in the transformer data collection project.\n\n \
+             Report any bugs or requests to clogan@nbpower.com.")
+app.setIcon("icon.ico")
+app.stopSubWindow()
+app.hideSubWindow("info")
+app.setIcon("icon.ico")
 app.go()
--- renamethings.py
+++ renamethings.py
@@ -1,6 +1,7 @@
 from openpyxl import Workbook
 from openpyxl import load_workbook
 import os, shutil
+
 def renstuff( spreadsheet, directory, namecol, platecol, txcol, prefix, offset ):
    # offset = 2
    # prefix='IMG0000'
@@ -16,37 +17,40 @@ def renstuff( spreadsheet, directory, namecol, platecol, txcol, prefix, offset )
     #a=ws[platecol +str(offset+ind)]
     n= ws.max_row
 
-    if not os.path.exists(directory+'\\processed\\'):
-        os.makedirs(directory+'\\processed\\')
+    if not os.path.exists(directory+'\\..\\processed\\'):
+        os.makedirs(directory+'\\..\\processed\\')
+    else:
+        shutil.rmtree(directory+'\\..\\processed\\')
+        os.makedirs(directory+'\\..\\processed\\')
     for x in range(1,n-offset):
         name=ws[namecol +str(offset+x)]
         platenum=ws[platecol +str(offset+x)]
         txnum=ws[txcol +str(offset+x)]
         print(name.value)
         if (platenum.value) and (txnum.value):
-            shutil.copyfile(directory+'\\raw\\'+prefix[0:(len(prefix)-len(str(platenum.value)))]+str(platenum.value)+'.jpg',directory+'\\processed\\'+name.value+' P.jpg')
+            shutil.copy(directory+'\\'+prefix[0:(len(prefix)-len(str(platenum.value)))]+str(platenum.value)+'.jpg',directory+'\\..\\processed\\'+name.value+' P.jpg')
 #           os.rename('raw\\'+prefix[0:(len(prefix)-len(str(platenum.value)))]+str(platenum.value)+'.jpg',name.value+' P.jpg')
-            shutil.copyfile(directory+'\\raw\\'+prefix[0:(len(prefix)-len(str(txnum.value)))]+str(txnum.value)+'.jpg',directory+'\\processed\\'+name.value+' T.jpg')
+            shutil.copy(directory+'\\'+prefix[0:(len(prefix)-len(str(txnum.value)))]+str(txnum.value)+'.jpg',directory+'\\..\\processed\\'+name.value+' T.jpg')
 
 #            os.rename('raw\\'+prefix[0:(len(prefix)-len(str(txnum.value)))]+str(txnum.value)+'.jpg',name.value+' T.jpg')
         elif (txnum.value) and (platenum.value == None):
             Tri='R'
-            shutil.copyfile(directory+'\\raw\\'+prefix[0:(len(prefix)-len(str(txnum.value)))]+str(txnum.value)+'.jpg',directory+'\\processed\\'+name.value+' T.jpg')
+            shutil.copy(directory+'\\'+prefix[0:(len(prefix)-len(str(txnum.value)))]+str(txnum.value)+'.jpg',directory+'\\..\\processed\\'+name.value+' T.jpg')
 
 #            os.rename('raw\\'+prefix[0:(len(prefix)-len(str(txnum.value)))]+str(txnum.value)+'.jpg',name.value+' T.jpg')
         elif (txnum.value == None) and platenum.value:
             if Tri=='R':
 #               os.rename('raw\\'+prefix[0:(len(prefix)-len(str(platenum.value)))]+str(platenum.value)+'.jpg',name.value+'.jpg')
-                shutil.copyfile(directory+'\\raw\\'+prefix[0:(len(prefix)-len(str(platenum.value)))]+str(platenum.value)+'.jpg',directory+'\\processed\\'+name.value+'.jpg')
+                shutil.copy(directory+'\\'+prefix[0:(len(prefix)-len(str(platenum.value)))]+str(platenum.value)+'.jpg',directory+'\\..\\processed\\'+name.value+'.jpg')
                 Tri = 'C'
             elif Tri=='C':
 #               os.rename('raw\\'+prefix[0:(len(prefix)-len(str(platenum.value)))]+str(platenum.value)+'.jpg',name.value+'.jpg')
-                shutil.copyfile(directory+'\\raw\\'+prefix[0:(len(prefix)-len(str(platenum.value)))]+str(platenum.value)+'.jpg',directory+'\\processed\\'+name.value+'.jpg')
+                shutil.copy(directory+'\\'+prefix[0:(len(prefix)-len(str(platenum.value)))]+str(platenum.value)+'.jpg',directory+'\\..\\processed\\'+name.value+'.jpg')
 
                 Tri = 'F'
             elif Tri=='F':
 #                os.rename('raw\\'+prefix[0:(len(prefix)-len(str(platenum.value)))]+str(platenum.value)+'.jpg',name.value+'.jpg')
-                shutil.copyfile(directory+'\\raw\\'+prefix[0:(len(prefix)-len(str(platenum.value)))]+str(platenum.value)+'.jpg',directory+'\\processed\\'+name.value+'.jpg')
+                shutil.copy(directory+'\\'+prefix[0:(len(prefix)-len(str(platenum.value)))]+str(platenum.value)+'.jpg',directory+'\\..\\processed\\'+name.value+'.jpg')
 
                 Tri = None
     return
